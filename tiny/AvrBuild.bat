bbc2ec903019199c39a75163f38c5314deb02f7f
Ivan Shakuta <ishakuta@gmail.com>
2014-07-07 00:26:14 +0300

    Remove old project

--- tiny/AvrBuild.bat
+++
@@ -1,2 +0,0 @@
-@ECHO OFF
-"C:\Program Files\Atmel\AVR Tools\AvrAssembler2\avrasm2.exe" -S "G:\tiny\labels.tmp" -fI -W+ie -C V2 -o "G:\tiny\tiny.hex" -d "G:\tiny\tiny.obj" -e "G:\tiny\tiny.eep" -m "G:\tiny\tiny.map" "G:\tiny\tiny.asm"
