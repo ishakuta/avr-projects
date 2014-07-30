190308c2db1555fb21d26e266a1aba83c7a4f135
Ivan Shakuta <ishakuta@gmail.com>
2014-07-31 00:16:56 +0300

    Add interrupts and basic handlers, buffers...
    - add rx/tx interrupts and basic handlers
    - buffers configuration

--- tiny-energy/tiny.asm
+++ tiny-energy/tiny.asm
@@ -1,26 +1,71 @@
 .include "tn2313def.inc"	; ATtiny2313
 .include "../lib/tinymacro.inc"
 
-; RAM =====================================================
+; RAM ========================================================
 .DSEG
-  
+	.equ MAXBUFF_IN 	=	10	; size in bytes, max size - 255
+	.equ MAXBUFF_OUT 	= 	10
+
+	versionPtr:		.byte 2 	; pointer to c_version constant string
+
+	IN_buff:	.byte	MAXBUFF_IN	; receive buffer
+	IN_PTR_S:	.byte	1			; start pointer
+	IN_PTR_E:	.byte	1			; end pointer
+	IN_FULL:	.byte	1			; overflow flag
+	 
+	OUT_buff:	.byte	MAXBUFF_OUT	; transmit buffer
+	OUT_PTR_S:	.byte	1			; start pointer
+	OUT_PTR_E:	.byte	1			; end pointer
+	OUT_FULL:	.byte	1			; overflow flag
+
+
 ; ENDRAM =====================================================  
 
 ; FLASH ===================================================
 .CSEG
-
 .include "interrupt-table.inc" ; interrupt table with handlers
-.include "../lib/coreinit.inc" 
 
 ; Internal Hardware Init  ======================================
+
+Reset:   	STACKINIT
+			RAMFLUSH
+
+; USART init
 	.equ 	XTAL        = 8000000 	
 	.equ 	baudrate    = 9600  
 	.equ 	bauddivider = XTAL/(16*baudrate)-1 ; 51.08
+
+uart_init:	LDI 	R16, low(bauddivider)
+			OUT 	UBRRL,R16
+			LDI 	R16, high(bauddivider)
+			OUT 	UBRRH,R16
+
+			LDI 	R16, 0 		; load 0 to R16
+			OUT 	UCSRA, R16  ; set 0 to UART Control and Status Register A
+
+			; receive/transmit enabled, rx/tx interrupts are enabled
+			LDI 	R16, (1<<RXEN)|(1<<TXEN)|(1<<RXCIE)|(1<<TXCIE)|(0<<UDRIE)
+			OUT 	UCSRB, R16
+
+			; Set frame format: 8 bit data (UCSZ*), 2 stop-bits (USBS)
+			; shift 3 (011) to left by UCSZ0 (1) byte = 0110
+			; this will set UCSZ0 and UCSZ1 to 1 - setting data size 8 bit
+			LDI 	R16, (1<<USBS)|(3<<UCSZ0) 
+			OUT 	UCSRC, R16
+
+; Init UART buffers - put 0 to all pointers
+			CLR		R16
+			STS		IN_PTR_S,  R16
+			STS		IN_PTR_E,  R16
+			STS		OUT_PTR_S, R16
+			STS		OUT_PTR_E, R16
+
+; enable interrupts globally
+			SEI
 ; End Internal Hardware Init ===================================
 
 
-; Run ==========================================================
-		RCALL 	uart_init
+; Run ==========================================================		
 
 ; End Run ======================================================
 
@@ -30,28 +75,24 @@ Main:   RCALL	uart_rcv	; wait for a byte
 		RCALL	uart_snd	; send it back
  
 		RJMP	Main
+
+; constants
+c_version:		.db	"ASM Telemetry v0.1",0
+
 ; End Main =====================================================
 
 
 ; Procedure ====================================================
 
-uart_init:	LDI 	R16, low(bauddivider)
-			OUT 	UBRRL,R16
-			LDI 	R16, high(bauddivider)
-			OUT 	UBRRH,R16
+loadConstAddrs:
+	LDI	R17, Low(2*c_version)	; get low byte
+	LDI	R18, High(2*c_version)	; get high byte
+ 
+	STS	versionPtr,   R17		; save low byte
+	STS	versionPtr+1, R18		; save high byte
+	RET
 
-			LDI 	R16,0 		; load 0 to R16
-			OUT 	UCSRA, R16  ; set 0 to UART Control and Status Register A
 
-			; receive/transmit enabled, interrupts are disabled
-			LDI 	R16, (1<<RXEN)|(1<<TXEN)|(0<<RXCIE)|(0<<TXCIE)|(0<<UDRIE)
-			OUT 	UCSRB, R16
-
-			; Set frame format: 8data
-			LDI 	R16, (1<<USBS)|(3<<UCSZ0)
-			OUT 	UCSRC, R16
-
-			RET
 
 uart_snd:	SBIS	UCSRA, UDRIE 	; skip if no flag: ready
 			RJMP	uart_snd 		; wait till ready - flag UDRE
